771d7ee94a0d3e0ee24168f87df3b866c26d6051
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-27 19:53:05 +0100

    wip

--- app/src/main/java/com/example/genremoviesincinema/ui/gallery/MovieData.java
+++ app/src/main/java/com/example/genremoviesincinema/ui/gallery/MovieData.java
@@ -1,4 +1,49 @@
 package com.example.genremoviesincinema.ui.gallery;
 
-public class MovieData {
+import java.io.Serializable;
+
+public class MovieData implements Serializable {
+    private  String name;
+    private int year;
+    private  String image;
+    private String  description;
+
+    public MovieData(String name, int year, String image, String description) {
+        this.name = name;
+        this.year = year;
+        this.image = image;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
