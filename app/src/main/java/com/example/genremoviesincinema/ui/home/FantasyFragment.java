c0e1f7ff135a56f075d7b971a0ebb0ec60080e85
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-28 15:30:36 +0100

    wip

--- app/src/main/java/com/example/genremoviesincinema/ui/home/FantasyFragment.java
+++ app/src/main/java/com/example/genremoviesincinema/ui/home/FantasyFragment.java
@@ -12,8 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.genremoviesincinema.DetailActivity;
 import com.example.genremoviesincinema.MovieAdapter;
 import com.example.genremoviesincinema.MovieSheet;
 import com.example.genremoviesincinema.R;
@@ -23,31 +25,48 @@ import java.util.List;
 
 public class FantasyFragment extends Fragment {
 
-  RecyclerView dRecyclerView;
+  RecyclerView mRecyclerView ;
   List<MovieData> fantasymovieList;
-  MovieSheet movieSheet;
+  MovieSheet moviesheet;
   MovieAdapter adapter;
 
-  public FantasyFragment() {
 
+  public FantasyFragment() {
+    // Required empty public constructor
   }
-  public static FantasyFragment newInstance(String str, String str2) {
+
+
+  // TODO: Rename and change types and number of parameters
+  public static FantasyFragment newInstance(String param1, String param2) {
     FantasyFragment fragment = new FantasyFragment();
     return fragment;
   }
-  @Override
-  public void OnCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    movieSheet = new MovieSheet();
 
-    fantasymovieList = movieSheet.getFantasyMovieList();
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    moviesheet = new MovieSheet();
+
+    fantasymovieList = moviesheet.getFantasyMovieList();
+    adapter = new MovieAdapter(fantasymovieList,getActivity());
+
     adapter.setOnAdapterListener(new MovieAdapter.CustomeAdapterListener() {
       @Override
       public void adapterListener(int position) {
-        MovieData movie = fantasymovieList.get(position) {
-          Intent intent = new Intent(getActivity(), )
-        }
+        MovieData movie = fantasymovieList.get(position);
+        Intent intent = new Intent(getActivity(),DetailActivity.class);
+        intent.putExtra(DetailActivity.MOVIE_OBJECT,movie);
+        startActivity(intent);
       }
     });
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_native_view, container, false);
+    mRecyclerView = view.findViewById(R.id.recyclerview);
+    mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    mRecyclerView.setAdapter(adapter);
+    return view;
   }
 }
