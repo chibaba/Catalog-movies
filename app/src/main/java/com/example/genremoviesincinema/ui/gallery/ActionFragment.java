91de4e779f730e6642ca9b2af31bb2ae708ee745
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-10-28 15:54:08 +0100

    wip

--- app/src/main/java/com/example/genremoviesincinema/ui/gallery/ActionFragment.java
+++ app/src/main/java/com/example/genremoviesincinema/ui/gallery/ActionFragment.java
@@ -1,5 +1,6 @@
 package com.example.genremoviesincinema.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,25 +12,60 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.genremoviesincinema.DetailActivity;
+import com.example.genremoviesincinema.MovieAdapter;
+import com.example.genremoviesincinema.MovieSheet;
 import com.example.genremoviesincinema.R;
+
+import java.util.List;
 
 public class ActionFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_detail, container, false);
-        final TextView textView = root.findViewById(R.id.all_fantasy);
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+    RecyclerView mRecyclerView ;
+    List<MovieData> actionmovieList;
+    MovieSheet moviesLab;
+    MovieAdapter adapter;
+
+    public ActionFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static ActionFragment newInstance() {
+        return new ActionFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        moviesLab = new MovieSheet();
+
+        actionmovieList = moviesLab.getActionMovieList();
+        adapter = new MovieAdapter(actionmovieList,getActivity());
+        adapter.setOnAdapterListener(new MovieAdapter.CustomeAdapterListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void adapterListener(int position) {
+                MovieData movie = actionmovieList.get(position);
+                Intent intent = new Intent(getActivity(),DetailActivity.class);
+                intent.putExtra(DetailActivity.MOVIE_OBJECT,movie);
+                startActivity(intent);
             }
         });
-        return root;
+
     }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_genericview, container, false);
+        mRecyclerView = view.findViewById(R.id.recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(adapter);
+        return view;
+    }
+
+
 }
